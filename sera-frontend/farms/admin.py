e638043a4e847aecd44c3ed0b67e84ef76f54773
denizkaya07 <denizkaya073432@gmail.com>
2026-03-28 09:10:22 +0300

    Remove duplicate frontend folder

--- sera-frontend/farms/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import Farm
-admin.site.register(Farm)
-# Register your models here.
--- sera-frontend/prescriptions/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-from .models import Prescription
-
-@admin.register(Prescription)
-class PrescriptionAdmin(admin.ModelAdmin):
-    list_display = ('id', 'title', 'created_by', 'created_at')
